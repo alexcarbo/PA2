56f01f802ac652e0a3d458284fbf0fd4a448fbc4
alexcarbo <32438651+alexcarbo@users.noreply.github.com>
2019-10-31 19:48:03 -0700

    added fade back

---
+++ fadeColorPicker.cpp
@@ -0,0 +1,16 @@
+#include "fadeColorPicker.h"
+
+fadeColorPicker::fadeColorPicker(double fade)
+{
+    fadeFactor = fade;
+}
+
+HSLAPixel fadeColorPicker::operator()(point p)
+{
+    Center c = p.c;
+    HSLAPixel colour = c.color;
+    int distSq = abs(c.x-p.x)**2 + abs(c.y-p.y)**2
+    double newL = colour.l * (fadeFactor ** distSq)
+    colour.l  = newL;
+    return colour;
+}
